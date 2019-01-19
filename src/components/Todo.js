d81de7ba2a35706f845708ceafaf7c274af623dd
Anthony Ng <newyork.anthonyng@gmail.com>
2019-01-18 23:53:33 -0500

    Add keyboard functionality to toggle todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,12 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { todoListItemStyle } from '../styles'
 
+const ENTER_KEY = 13
+const SPACE_KEY = 32
+
 const Todo = ({ onClick, completed, text }) => (
   <li
     onClick={onClick}
     style={{
       textDecoration: completed ? 'line-through' : 'none',
       ...todoListItemStyle
+    }}
+    tabIndex="0"
+    onKeyDown={({ keyCode }) => {
+      if (keyCode === ENTER_KEY || keyCode === SPACE_KEY) {
+        onClick()
+      }
     }}
   >
     {completed ? '⚫' : '⚪'}
